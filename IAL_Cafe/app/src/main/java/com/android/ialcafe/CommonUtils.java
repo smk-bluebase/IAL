d337210cd8c7b065b47d31cee1eb689ee6721190
Mohana Krishnan S <mohanakrishnan.s@bluebase.in>
2020-10-22 21:16:33 +0530

    New File Structure

--- IAL_Cafe/app/src/main/java/com/android/ialcafe/CommonUtils.java
+++
@@ -1,9 +0,0 @@
-package com.android.ialcafe;
-
-public class CommonUtils {
-//    static String IP = "http://192.0.2.72";
-    static String IP = "http://192.168.201.165";
-//    static String IP = "http://192.168.1.100";
-}
-
-
